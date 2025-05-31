3d220f37e1edf626edbc20965e896b6d456ba40b
AzimKorkmaz <azimkorkmaz@gmail.com>
2025-05-31 21:13:52 +0300

    added

--- src/test/java/TMDB/MovieList.java
+++ src/test/java/TMDB/MovieList.java
@@ -1,6 +1,19 @@
 package TMDB;
 
 import Utility.Parent;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class MovieList extends Parent {
+    @Test
+    public void getNowPlaying() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("/movie/now_playing")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
 }
