5b3adbd9174947fa5b2bed9839c75de29259e237
theeren1905 <erenozzbbk@gmail.com>
2025-05-31 21:51:46 +0300

    added

--- src/test/java/TMDB/MovieList.java
+++ src/test/java/TMDB/MovieList.java
@@ -16,4 +16,38 @@ public class MovieList extends Parent {
                 .log().body()
                 .statusCode(200);
     }
+
+    @Test
+    public void getPopular() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("/movie/popular")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+
+    @Test
+    public void getTopRated() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("/movie/top_rated")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public void getUpcoming() {
+        given()
+                .spec(reqSpec)
+                .when()
+                .get("/movie/upcoming")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
 }
