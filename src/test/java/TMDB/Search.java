e7b2e84e550e5aabbbec9696303abdac508e83eb
TugbaKilic <tugbakilic3302@gmail.com>
2025-05-31 22:08:56 +0300

    Search done.

--- src/test/java/TMDB/Search.java
+++ src/test/java/TMDB/Search.java
@@ -1,6 +1,61 @@
 package TMDB;
 
 import Utility.Parent;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class Search extends Parent {
+    @Test
+    public void getMovie(){
+        given()
+                .spec(reqSpec)
+                .queryParam( "query", "Hot Frosty")
+                .when()
+                .get("/search/movie")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public void getTV(){
+        given()
+                .spec(reqSpec)
+                .queryParam( "query", "Watch What Happens Live with Andy Cohen")
+                .when()
+                .get("/search/tv")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public  void getPerson(){
+        given()
+                .spec(reqSpec)
+                .queryParam( "query", "Jason Statham")
+                .when()
+                .get("/search/person")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+    @Test
+    public void getkeyword(){
+        given()
+                .spec(reqSpec)
+                .queryParam( "query", "TV")
+                .when()
+                .get("/search/keyword")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+
+
+
+
+
 }
