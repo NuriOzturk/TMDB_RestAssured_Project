db31003a00d4eb8dd941a0eace70842af5ead97a
rftbtr <rifatbatir@gmail.com>
2025-05-31 23:30:32 +0300

    Added

--- src/test/java/Utility/ConfigReader.java
+++ src/test/java/Utility/ConfigReader.java
@@ -1,20 +1,46 @@
 package Utility;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigReader {
+    private static final String configFilePath = "configuration.properties";
     private static Properties properties = new Properties();
-
     static {
         try {
-            FileInputStream file = new FileInputStream("configuration.properties");
+            FileInputStream file = new FileInputStream(configFilePath);
             properties.load(file);
             file.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void saveProperties() {
+        try (FileOutputStream outputFile = new FileOutputStream(configFilePath)) {
+            properties.store(outputFile, "Updated Dynamic Properties");
+        } catch (IOException e) {
+            throw new RuntimeException("Error while updating configuration.properties file", e);
+        }
+    }
+
+    public static void saveToConfig(String key, String value) {
+        properties.setProperty(key, value);
+        saveProperties();
+    }
+
+    public static void updateProperty(String keyword) {
+        switch (keyword) {
+            case "movieID":
+                String movieID="";
+                properties.setProperty(keyword, movieID);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported property key: " + keyword);
+        }
+        saveProperties();
     }
 
     public static String getProperty(String keyword) {
